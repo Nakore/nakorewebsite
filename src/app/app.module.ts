205eec8a22144978d1c32ac1c61d4c8f7d3db923
Netpoc <ayenotopeter@hotmail.com>
2023-04-03 20:09:04 +0000

    initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app-core/header/header.component';
+import { FooterComponent } from './app-core/footer/footer.component';
+import { ContentComponent } from './app-core/content/content.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: ContentComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/typings.d.ts
@@ -0,0 +1,1 @@
+declare var actionScr:any;
